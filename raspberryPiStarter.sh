275b98130bcd0d6743df969de730481dd9789c67
the-creepy-1 <chakraborty.soumik23@gmail.com>
2020-12-07 14:13:17 +0530

    latest

--- raspberryPiStarter.sh
+++ raspberryPiStarter.sh
@@ -1,7 +1,8 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y 
 sudo apt-get install roxterm pidgin deluge -y
-sudo apt-get install php7.3 php7.3-curl php7.3-mysql
+sudo apt-get install php7.3 php7.3-curl php7.3-mysql -y
+sudo apt-get install vim vifm -y
 mkdir ~/.vim
 cd ~/.vim
 
