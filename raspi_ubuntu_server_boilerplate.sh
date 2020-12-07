ecf8981d86fc7d6734168f093e5f51b0cb464ce9
soumik <chakraborty.soumik23@gmail.com>
2020-12-07 16:57:08 +0000

    server boilerplate added to repo

---
+++ raspi_ubuntu_server_boilerplate.sh
@@ -0,0 +1,6 @@
+sudo apt-get install net-tools -y
+sudo apt-get install php7.4 php7.4-mysql php7.4-zip php7.4-curl -y
+sudo apt-get install vim vifm -y
+sudo apt-get install apache2 mysql-server -y
+sudo apt-get install neovim -y
+
